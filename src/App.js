d4932d0dfb118fc441a191a217d63fc299fd1f89
akhilbidhuri <akhilbidhuri@gmail.com>
2019-04-01 18:33:30 +0530

    home

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Route, BrowserRouter as Router} from 'react-router-dom';
+import Home from './components/home'
+const routes=()=>(
+<Router>
+<Route path="/" exact component={Home}/>
+</Router>
+);
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
 
-export default App;
+export default routes;
---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Row} from 'simple-flexbox'
+import './main.css';
+const home=()=>(
+        <div className="back" style={{background:'linear-gradient(to right bottom, #00695c,#26a69a)'}}>
+            <Row vertical="center" horizontal="center">
+            <h1 className="heading" style={{color:'#004d40',textAlign:'center'}}>Ukaa</h1>
+            </Row>
+        </div>
+);
+ 
+
+export default home;
